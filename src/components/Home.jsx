b988940ec6245f71d258ad185a01d8cc110ade3d
beejay321 <jibodu.busola@gmail.com>
2021-12-05 17:13:14 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,39 @@
 import { Container, Row, Col, Image } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [artist, setArtist] = useState();
+  const [query, setQuery] = useState("eminem");
+
+  useEffect(() => {
+    const getArtist = async () => {
+      try {
+        let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`);
+        console.log(response);
+        if (response.ok) {
+          let result = await response.json();
+          console.log(result.data);
+          setArtist(result.data);
+        }
+      } catch (error) {
+        console.log("This is the error", error);
+      }
+    };
+    getArtist("query");
+  }, []);
+
   return (
     <>
       <Container fluid>
         <Row className="mt-4 mx-5 ">
-          <Col className="my-3  d-flex justify-content-center ">
-            <div className="">
-              <Image className="cover d-flex justify-content-center" src="" />
-            </div>
-          </Col>
+          {artist &&
+            artist.map((album) => (
+              <Col xs={10} sm={6} md={6} lg={4} xl={4} className="my-3  d-flex justify-content-center ">
+                <div className="coverDiv">
+                  <Image className="cover d-flex justify-content-center" src={album.album.cover_big} rounded />
+                </div>
+              </Col>
+            ))}
         </Row>
       </Container>
     </>
