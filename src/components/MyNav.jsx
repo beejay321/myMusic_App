44af78e97ac2956583437477f01601ce39351790
beejay321 <jibodu.busola@gmail.com>
2021-12-06 15:27:13 +0100

    Update MyNav.jsx

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,20 +1,18 @@
-import { Navbar, Container, Form, FormControl } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+
 import { Link } from "react-router-dom";
 
-const MyNav = () => (
-  <>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container fluid>
-        <Link to="/">
-          <Navbar.Brand>MusicApp</Navbar.Brand>
-        </Link>{" "}
-        <Form className="d-flex ">
-          <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-          <button>Search</button>
-        </Form>
-      </Container>
-    </Navbar>
-  </>
-);
-
+const MyNav = () => {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container fluid>
+          <Link to="/">
+            <Navbar.Brand>MusicApp</Navbar.Brand>
+          </Link>{" "}
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 export default MyNav;
