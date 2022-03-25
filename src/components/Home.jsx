80bd1683a98b0a2af81a7868450bf2690d555683
beejay321 <jibodu.busola@gmail.com>
2022-03-25 16:53:45 +0100

    Update Home.jsx
    Cleaning up code
    Taking out the imagemodal component, taking out the get album function, updating style of the component. Implementing the useNavigate function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,34 @@
 import { Container, Row, Col, Image, Form, FormControl, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import ImageModal from "./ImageModal";
+import { useNavigate } from "react-router-dom";
 
-const Home = () => {
+const Home = (props) => {
   const [artist, setArtist] = useState([]);
   const [query, setQuery] = useState("");
-  const [modalShow, setModalShow] = useState(false);
-  const [selectedAlbum, setSelectedAlbum] = useState(artist[0]);
+  let navigate = useNavigate();
 
   useEffect(() => {
-    const onLoad = async () => {
+    const onMount = async () => {
       try {
-        let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem`);
+        let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=rock`);
         if (response.ok) {
           let result = await response.json();
+          console.log(result.data);
           setArtist(result.data);
         }
       } catch (error) {
         console.log("This is the error", error);
       }
     };
-    onLoad();
+    onMount();
   }, []);
 
-  const getArtist = async (query) => {
+  const search = async (query) => {
     try {
       let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`);
       if (response.ok) {
         let result = await response.json();
+        console.log(result.data);
         setArtist(result.data);
       }
     } catch (error) {
@@ -35,47 +36,41 @@ const Home = () => {
     }
   };
 
-  const getAlbum = async (album) => {
-    let id = album.album.id;
-
-    try {
-      let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/` + id);
-      if (response.ok) {
-        let result = await response.json();
-        setSelectedAlbum(result);
-      }
-    } catch (error) {
-      console.log("This is the error", error);
-    }
-  };
-
-  const handleSelectedAlbum = (album, index) => {
-    setModalShow(true);
-    getAlbum(album);
+  const handleSelectedAlbum = (album) => {
+    navigate(`/album/${album.album.id}`, { replace: true });
   };
 
   return (
     <>
-      <Container fluid>
-        <Form className="d-flex mx-3 py-3">
-          <FormControl type="search" placeholder="Search By Artist" className="me-2" aria-label="Search" value={query} onChange={(e) => setQuery(e.currentTarget.value.toLowerCase())} />
-          <Button onClick={() => getArtist(query)} variant="outline-success">
-            Search
-          </Button>
-        </Form>
-
+      <Container fluid className="home">
+        <Row className="d-flex justify-content-end">
+          <Form className="search d-flex mx-2 py-3  ">
+            <FormControl
+              type="search"
+              placeholder="What are you looking for?"
+              className="searchForm me-2 "
+              aria-label="Search"
+              value={query}
+              onChange={(e) => setQuery(e.currentTarget.value.toLowerCase())}
+            />
+            <Button className="searchBtn " onClick={() => search(query)} variant="outline-success">
+              Search
+            </Button>
+          </Form>
+        </Row>
         <Row className=" m-1 ">
           {artist &&
-            artist.map((album) => (
-              <Col xs={10} sm={6} xl={4} className="my-1  px-0 d-flex justify-content-center ">
-                <div className="coverDiv">
-                  <Image className="cover d-flex justify-content-center" src={album.album.cover_big} rounded onClick={() => handleSelectedAlbum(album)} />
+            artist.map((album, i) => (
+              <Col key={i} xs={10} sm={6} xl={2} className="my-1  px-0 d-grid justify-content-center ">
+                <div className="albumDiv mx-1" onClick={() => handleSelectedAlbum(album)}>
+                  {/* <div className="coverDiv"> */}
+                    <Image className="cover " src={album.album.cover_big} rounded />
+                    <p className="coverTitle px-2  py-3 ">{album.album.title}</p>
+                  {/* </div> */}
                 </div>
               </Col>
             ))}
         </Row>
-
-        <ImageModal show={modalShow} onHide={() => setModalShow(false)} selectedAlbum={selectedAlbum} />
       </Container>
     </>
   );
