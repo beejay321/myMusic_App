1b056dd7ad8aa187fd72f823b7a11cbb2073cf1c
beejay321 <jibodu.busola@gmail.com>
2022-03-25 17:06:00 +0100

    Update MyNav.jsx
    Change app icon

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Container } from "react-bootstrap";
-
+import logo from "../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 const MyNav = () => {
@@ -8,7 +8,10 @@ const MyNav = () => {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container fluid>
           <Link to="/">
-            <Navbar.Brand>MyMusic</Navbar.Brand>
+            <Navbar.Brand>
+              <img alt="music_icon" src={logo} width="50" className="d-inline-block align-top mx-2 py-1 " />
+              {/* MyMusic */}
+            </Navbar.Brand>
           </Link>{" "}
         </Container>
       </Navbar>
