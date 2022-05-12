6cbc77b18568ab97994e0a234c2faa9830c86634
beejay321 <jibodu.busola@gmail.com>
2022-05-12 18:14:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MyNav from "./components/MyNav";
 import Home from "./components/Home";
 import Album from "./components/Album";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
+
+
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <MyNav />
-
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/album/:id" element={<Album />} />
-          
-        </Routes>
-      </BrowserRouter>
+      <div className="App">
+        <BrowserRouter>
+          <div className="d-flex">
+            <Sidebar />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/album/:id" element={<Album />} />
+            </Routes>
+            {/* <Sidebar /> */}
+          </div>
+        </BrowserRouter>
+      </div>
     </>
   );
 };
